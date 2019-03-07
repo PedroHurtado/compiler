4cb42344842392feb732fb132d02fb2015d7e22d
PedroHurtado <pedrohurt@gmail.com>
2019-03-07 18:51:03 +0100

    add wrapper

--- x.js
+++ x.js
@@ -273,3 +273,13 @@ function noopBlock() {
     d: noop
   };
 }
+
+export default function App(target,data){
+  Object.assign(this,data);
+  this.fragment = render()(this);
+  this.fragment.c();
+  this.fragment.m(target,null);
+}
+App.prototype.set = function(data){
+ this.fragment.u({},data)
+}
