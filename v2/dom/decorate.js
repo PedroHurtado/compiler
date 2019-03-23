804c41502bc2338d4116724849835513588ba790
PedroHurtado <pedrohurt@gmail.com>
2019-03-23 10:46:16 +0100

    decorate inputs and outputs

--- v2/dom/decorate.js
+++ v2/dom/decorate.js
@@ -1,5 +1,5 @@
 function changes(data, instance) {
-    if(!data) return;
+    if (!data) return;
     let changes = false;
     for (let key in data) {
         let change =
@@ -22,12 +22,44 @@ function set(render) {
             render(this);
         } else {
             this.first = 0;
-            if (changes(data, this){
+            if (changes(data, this)){
                 render(this);
             }
         }
     }
 }
+function decorateOutputs(ctor) {
+    let outputs = ctor.outputs;
+    if (Array.isArray(outputs)) {
+        outputs.forEach(output => {
+            Object.defineProperty(ctor.prototype, output, {
+                get: function () {
+                    return this[`_${output}`] || function(){};
+                },
+                set: function (value) {
+                    this[`_${output}`] = value;
+                }
+            })
+        })
+    }
+}
+function decorateDefaultValues(ctor){
+    let inputs = ctor.inputs;
+    if(inputs && typeof inputs === 'object'){
+        Object.keys(inputs).forEach(input=>{
+            Object.defineProperty(ctor.prototype,input,{
+                get:function(){
+                    return this[`_${input}`];
+                },
+                set:function(value){
+                    this[`_${input}`] = value;
+                }
+            })
+        })
+    }
+}
 export function decorate(ctor, render) {
     ctor.prototype.set = set(render);
+    decorateOutputs(ctor);
+    decorateDefaultValues(ctor);
 }
