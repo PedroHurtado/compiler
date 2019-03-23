694bc6d23ab62e84e5035fc6854532cf158b1e1e
PedroHurtado <pedrohurt@gmail.com>
2019-03-23 22:00:44 +0100

    refactor decorate

--- v2/dom/decorate.js
+++ v2/dom/decorate.js
@@ -1,15 +1,17 @@
 import {set} from './changes.js';
-
+const noop = function(){};
 function decorateOutputs(ctor) {
     let outputs = ctor.outputs;
     if (Array.isArray(outputs)) {
+        let attribute = 
         outputs.forEach(output => {
+            let attribute = `_${output}`
             Object.defineProperty(ctor.prototype, output, {
                 get: function () {
-                    return this[`_${output}`] || function(){};
+                    return this[attribute] || noop;
                 },
                 set: function (value) {
-                    this[`_${output}`] = value;
+                    this[attribute] = value;
                 }
             })
         })
@@ -19,14 +21,16 @@ function decorateInputs(ctor){
     let inputs = ctor.inputs;
     if(inputs && typeof inputs === 'object'){
         Object.keys(inputs).forEach(input=>{
+            let attribute = `_${input}`
             Object.defineProperty(ctor.prototype,input,{
                 get:function(){
-                    return this[`_${input}`];
+                    return this[attribute];
                 },
                 set:function(value){
-                    this[`_${input}`] = value;
+                    this[attribute] = value;
                 }
-            })
+            });
+            ctor.prototype[attribute] = inputs[input];
         })
     }
 }
