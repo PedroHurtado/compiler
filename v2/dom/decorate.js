519bbc1d32739e644a31aa4234bbe4b1dda40855
PedroHurtado <pedrohurt@gmail.com>
2019-03-25 12:14:41 +0100

    create fields in decorateOutputs

--- v2/dom/decorate.js
+++ v2/dom/decorate.js
@@ -14,6 +14,7 @@ function decorateOutputs(ctor) {
                     this[attribute] = value;
                 }
             })
+            ctor.prototype[attribute] = noop;
         })
     }
 }
