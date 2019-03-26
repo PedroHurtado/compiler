94e239e9cdba6874b8242741f27f7c1737da0df7
PedroHurtado <pedrohurt@gmail.com>
2019-03-26 21:22:05 +0100

    add anchor in bootStrap

--- v2/dom/bootstrap.js
+++ v2/dom/bootstrap.js
@@ -1,20 +1,12 @@
-import {create,append} from './domfunctions.js';
-import {getConstructor} from './customelements.js';
-export function bootStrap(parent,data={}){
-    let {tag} = this;
-    let ctor = getConstructor(tag);
-    let node =create(tag,0);
-    if(!ctor.customElement){
-        let instance = new ctor();
-        instance.__node = node;
-        if('set' in instance){
-            instance.set(data);
-        }
+import { create, append } from './domfunctions.js';
+import { getConstructor } from './customelements.js';
+export function bootStrap(parent,data = {},anchor=null) {
+    let { tag } = this;
+    let { customElement } = getConstructor(tag);
+    let instance = new this();
+    if (!customElement) {
+        instance.__node = create(tag, 0);
     }
-    else{
-        if('set' in node){
-            node.set(data);
-        }
-    }
-    append(parent,node,parent.firstChild);
+    instance.set(data);
+    append(parent, instance.__node || instance, anchor || parent.firstChild);
 }
