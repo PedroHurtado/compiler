28a021d19fbbb1aca51add7d3befad36aa512fc2
PedroHurtado <pedrohurt@gmail.com>
2019-04-03 12:49:50 +0200

    implements onChanges

--- v2/dom/changes.js
+++ v2/dom/changes.js
@@ -1,17 +1,25 @@
+function onChanges(instance,property,newValue,oldVale){
+    if('onChanges' in instance ){
+        instance.onChanges(property,newValue,oldVale);
+    }
+}
 export function changes(data, instance) {
     if (!data) return;
-    let changes = false;
+    let dirty = false;
     for (let key in data) {
         let change =
             instance[key] !== data[key] ||
             typeof data[key] === "object" ||
             typeof data[key] === "function";
         if (change) {
-            changes = change;
+            dirty=true;
+            let newValue = data[key];
+            let oldVale = instance[key];
+            onChanges(instance,key,newValue,oldVale);
             instance[key] = data[key];
         }
     }
-    return changes;
+    return dirty;
 }
 
 export function set(render) {
